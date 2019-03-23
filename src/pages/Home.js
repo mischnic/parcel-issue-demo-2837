46e0a7868b0a314e755808705a88ffe166405b58
zhaoyao91 <zhaoyao91@gmail.com>
2019-03-23 23:47:22 +0800

    introduce antd-mobile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 // @flow
 
 import * as React from "react";
+import { Button } from "antd-mobile";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Button>Antd Mobile</Button>
     </div>
   );
 };
